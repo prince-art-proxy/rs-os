7d04a552f4ccb29d6d196116094c12001e235698
BestSpark687090 <73793381+BestSpark687090@users.noreply.github.com>
2026-04-28 22:06:21 -0500

    Update proxy prefix path in index.js

--- public/proxy/index.js
+++ public/proxy/index.js
@@ -28,7 +28,7 @@ const scramjet = new ScramjetController({
 		all: '/proxy/scram/scramjet.all.js',
 		sync: '/proxy/scram/scramjet.sync.js',
 	},
-	prefix: "/proxy/scramjet/"
+	prefix: "/proxy/"
 });
 
 scramjet.init();

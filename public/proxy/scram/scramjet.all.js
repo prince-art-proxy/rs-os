e8588fcdfd3369e8e39265a91b136acf363eda0f
RS-OS <241219834+prince-art-proxy@users.noreply.github.com>
2026-05-09 20:37:49 -0500

    Change scramjet prefix from '/proxy/scramjet/' to '/proxy/scram/'

--- public/proxy/scram/scramjet.all.js
+++ public/proxy/scram/scramjet.all.js
@@ -3070,7 +3070,7 @@
               },
               r = t(
                 {
-                  prefix: "/proxy/scramjet/",
+                  prefix: "/proxy/scram/",
                   globals: {
                     wrapfn: "$scramjet$wrap",
                     wrappropertybase: "$scramjet__",
